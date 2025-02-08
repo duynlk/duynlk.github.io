34d4e850324eaa200c04fe3c301d024e8b493da9
DuyNLK <nguyenlukhanhduy@gmail.com>
2025-02-08 14:15:32 +0700

    add form rsvp

--- wedding/js/main.js
+++ wedding/js/main.js
@@ -1,31 +1,7 @@
 (function ($) {
     "use strict";
 
-    // Navbar on scrolling
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 200) {
-            $('.navbar').fadeIn('slow').css('display', 'flex');
-        } else {
-            $('.navbar').fadeOut('slow').css('display', 'none');
-        }
-    });
-
-
-    // Smooth scrolling on the navbar links
-    $(".navbar-nav a").on('click', function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            
-            $('html, body').animate({
-                scrollTop: $(this.hash).offset().top - 45
-            }, 1500, 'easeInOutExpo');
-            
-            if ($(this).parents('.navbar-nav').length) {
-                $('.navbar-nav .active').removeClass('active');
-                $(this).closest('a').addClass('active');
-            }
-        }
-    });
+    var isShowRsvp = false;
 
     // Scroll to Bottom
     $(window).scroll(function () {
@@ -34,22 +10,15 @@
         } else {
             $('.scroll-to-bottom').fadeIn('slow');
         }
+
+        if ($(window).scrollTop() + $(window).height() >= $(document).height() - 200) {
+            if (!isShowRsvp){
+                $('#rsvpModal').modal('show');
+                isShowRsvp = true;
+            }
+        }
     });
 
-
-    // Portfolio isotope and filter
-    var portfolioIsotope = $('.portfolio-container').isotope({
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-    });
-    $('#portfolio-flters li').on('click', function () {
-        $("#portfolio-flters li").removeClass('active');
-        $(this).addClass('active');
-
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
-    });
-    
-    
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 200) {
@@ -62,7 +31,6 @@
         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
         return false;
     });
-
 
     // Gallery carousel
     $(".gallery-carousel").owlCarousel({
@@ -121,5 +89,27 @@
     changeImageSrc();
     $(window).resize(function() {
         changeImageSrc();
+    });
+
+    $('#submitRsvp').on('click', function(){
+        var nameRsvp = $('#nameRsvp').val();
+        var commitRsvp = 'Sẽ đến';
+        if ($('#nameRsvp').val() == '0'){
+            commitRsvp = 'Không đến được';
+        }
+        var msgRsvp = $('#msgRsvp').val();
+
+        $.ajax({
+            url: "https://formsubmit.co/ajax/nguyenlukhanhduy@gmail.com",
+            method: "POST",
+            data: {
+                name: nameRsvp + ' - ' + commitRsvp,
+                message: msgRsvp
+            },
+            dataType: "json",
+            accepts: 'application/json',
+            success: (data) => console.log(data),
+            error: (err) => console.log(err)
+        });
     });
 })(jQuery);
